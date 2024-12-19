241cf5aa21d019815d32c4b7d8763290f511d0bf
Facundo Villa <fvilla@netnix.net>
2024-12-19 00:25:33 -0300

    add package main

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Svate, getSvate } from "$lib/index.svelte.ts";
+import SvateContextProvider from "$lib/SvateContextProvider.svelte";
+
+export default {
+	Svate,
+	SvateContextProvider,
+	getSvate,
+};
